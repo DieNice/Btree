ffb24ea8427ee34518caa37803f2b64f6d279d2c
DieNice <DailyPlanning@bk.ru>
2019-12-05 20:06:43 +1000

    all tested all done

--- main.cpp
+++ main.cpp
@@ -3,21 +3,6 @@
 using namespace std;
 
 int main() {
-    BTree<int> tree(2);
 
-    int n = 11;
-    float d = 3.14;
-    char h = 'a';
-    for (int i = 1; i < n; i++) {
-        tree.add(n-i);
-        d = i % 5;
-        h++;
 
-    }
-    tree.print();
-
-    tree.del(10);
-
-    cout << "\n--------" << endl;
-    tree.print();
 }
